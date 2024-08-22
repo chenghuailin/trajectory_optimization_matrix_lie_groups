095eab4a1cb44159ade1bc65469ace4717602c15
chenghuailin <chadwick0318@gmail.com>
2024-08-22 09:32:04 +0200

    Create err-state cost and its ddp file

---
+++ traoptlibrary/main_errSE3ddp.py
@@ -0,0 +1,201 @@
+from traopt_controller import iLQR
+import numpy as np
+from jax import random
+from traopt_dynamics import ErrorStateSE3AutoDiffDynamics
+from traopt_cost import ErrorStateSE3LieAlgebraAutoDiffQuadraticCost
+from traopt_utilis import skew, unskew, se3_hat
+from scipy.linalg import expm
+from pyquaternion import Quaternion
+import matplotlib.pyplot as plt
+# from scipy.spatial.transform import Rotation as R
+
+seed = 24234156
+key = random.key(seed)
+
+dt = 0.01
+Nsim = 400   # Simulation horizon
+
+# ====================
+# Inertia Matrix
+# ====================
+
+m = 1
+Ib = np.diag([ 0.5,0.7,0.9 ])
+J = np.block([
+    [Ib, np.zeros((3, 3))],
+    [np.zeros((3, 3)), m * np.identity(3)]
+])
+
+# =====================================================
+# Reference Generation
+# =====================================================
+
+q0_ref = np.array([1, 0, 0, 0])
+p0_ref = np.array([0, 0, 0])
+w0_ref = np.array([0, 0, 1]) * 1
+v0_ref = np.array([1, 0, 0.1]) * 2
+
+# x0_ref and X should be kept the same
+x0_ref = np.concatenate((q0_ref, p0_ref))
+# X = np.eye(4) # SE(3)
+X0 = np.block([
+    [ Quaternion(q0_ref).rotation_matrix, p0_ref.reshape(-1,1) ],
+    [ np.zeros((1,3)),1 ],
+])
+X = X0.copy()
+
+xid_ref = np.concatenate((w0_ref, v0_ref))
+
+X_ref = np.zeros((Nsim + 1, 7, 1))  # 7 because of [quat(4) + position(3)]
+xi_ref = np.zeros((Nsim + 1, 6, 1)) 
+
+X_ref[0] = x0_ref.reshape(7,1)
+xi_ref[0] = xid_ref.reshape(6,1)
+
+for i in range(Nsim):
+
+    xid_ref_rt = xid_ref.copy()
+
+    # You can try some time-varying twists here:
+    # xid_ref_rt[0] = np.sin(i / 20) * 2
+    # xid_ref_rt[4] = np.cos(np.sqrt(i)) * 1
+    # xid_ref_rt[5] = 1  # np.sin(np.sqrt(i)) * 1
+
+    Xi = np.block([
+        [skew(xid_ref_rt[:3]), xid_ref_rt[3:6].reshape(3, 1)],
+        [np.zeros((1, 3)), 0]
+    ])
+
+    X = X @ expm(Xi * dt)
+
+    # Extract rotation matrix and position vector
+    rot_matrix = X[:3, :3]
+    position = X[:3, 3]
+
+    # Convert rotation matrix to quaternion
+    quaternion = Quaternion(matrix=rot_matrix)
+    quat = quaternion.elements
+
+    # Store the reference trajectory (quaternion + position)
+    X_ref[i + 1] = np.concatenate((quat, position)).reshape(7,1)
+
+    # Store the reference twists
+    xi_ref[i + 1] = xid_ref_rt.reshape(6,1)
+
+
+# =====================================================
+# Dynamics Instantiation
+# =====================================================
+
+debug = {"vel_zero": False}
+dyn_se3 = ErrorStateSE3AutoDiffDynamics(J, X_ref, xi_ref, dt, debug=debug)
+
+# =====================================================
+# Cost Instatiation
+# =====================================================
+
+Q = np.diag([
+    10., 10., 10., 1., 1., 1.,
+    1., 1., 1., 1., 1., 1.
+])
+P = np.diag([
+    10., 10., 10., 1., 1., 1.,
+    1., 1., 1., 1., 1., 1.
+]) * 10
+R = np.identity(6) * 1e-5
+
+cost = ErrorStateSE3LieAlgebraAutoDiffQuadraticCost( Q,R,P, xi_ref )
+
+# =====================================================
+# Visualization with Vector
+# =====================================================
+
+# interval_plot = int((Nsim + 1) / 40)
+# lim = 5
+
+# # Initialize the plot
+# fig1 = plt.figure(1)
+# ax1 = fig1.add_subplot(121, projection='3d')
+# ax2 = fig1.add_subplot(122, projection='3d')
+
+# # Define an initial vector and plot on figure
+# initial_vector = np.array([1, 0, 0])  # Example initial vector
+# ax1.quiver(0, 0, 0, initial_vector[0], initial_vector[1], initial_vector[2], color='g', label='Initial Vector')
+# ax2.quiver(0, 0, 0, initial_vector[0], initial_vector[1], initial_vector[2], color='g', label='Initial Vector')
+
+# # Loop through quaternion data to plot rotated vectors
+# for i in range(0, Nsim + 1, interval_plot):  
+
+#     # =========== 1. Plot the reference trajectory ===========
+
+#     quat = Quaternion(X_ref[i, :4, 0])  # Extract the quaternion from the X_ref data
+#     rot_matrix = quat.rotation_matrix  # Get the rotation matrix from the quaternion
+#     rotated_vector = rot_matrix @ initial_vector  # Apply the rotation to the initial vector
+    
+#     # Extract the position 
+#     position = X_ref[i, 4:, 0]
+#     rotated_vector = rotated_vector + position
+
+#     # Plot the rotated vector
+#     ax1.quiver(position[0], position[1], position[2],
+#               rotated_vector[0], rotated_vector[1], rotated_vector[2],
+#               color='b', length=1, label='Rotated Vector' if i == 0 else '')
+
+#     # =========== 2. Plot the simulated velocity ===========
+    
+#     # se3_matrix = expm( se3_hat( x_sim_list[i, 6:, :] ))
+#     # rot_matrix = se3_matrix[:3,:3]  # Get the rotation matrix from the quaternion
+#     # rotated_vector = rot_matrix @ initial_vector  # Apply the rotation to the initial vector
+
+#     # position = se3_matrix[:3, 3]
+#     # rotated_vector = rotated_vector + position
+    
+#     # # Plot the rotated vector
+#     # ax2.quiver(position[0], position[1], position[2],
+#     #           rotated_vector[0], rotated_vector[1], rotated_vector[2],
+#     #           color='b', length=1, label='Rotated Vector' if i == 0 else '')
+    
+#     # =========== 3. Plot the simulated error-state configuration trajectory ===========
+
+#     se3_matrix = np.block([
+#         [Quaternion(X_ref[i, :4]).rotation_matrix, X_ref[i, 4:].reshape(3, 1)],
+#         [ np.zeros((1,3)), 1 ],
+#     ]) @ expm( se3_hat(x_sim_list[i, :6, :]) )
+    
+#     rot_matrix = se3_matrix[:3,:3]  # Get the rotation matrix from the quaternion
+#     rotated_vector = rot_matrix @ initial_vector  # Apply the rotation to the initial vector
+
+#     position = se3_matrix[:3, 3]
+#     rotated_vector = rotated_vector + position
+    
+#     # Plot the rotated vector
+#     ax2.quiver(position[0], position[1], position[2],
+#               rotated_vector[0], rotated_vector[1], rotated_vector[2],
+#               color='b', length=1, label='Rotated Vector' if i == 0 else '')
+
+
+# # Set the limits for the axes
+
+# ax1.set_xlim([-lim, lim]) 
+# ax1.set_ylim([-lim, lim])
+# ax1.set_zlim([-lim, lim])
+# ax1.legend()
+# ax1.set_xlabel('X')
+# ax1.set_ylabel('Y')
+# ax1.set_zlabel('Z')
+
+# ax2.set_xlim([-lim, lim])  
+# ax2.set_ylim([-lim, lim])
+# ax2.set_zlim([-lim, lim])
+# ax2.legend()
+# ax2.set_xlabel('X')
+# ax2.set_ylabel('Y')
+# ax2.set_zlabel('Z')
+
+
+# # =====================================================
+# # Plotting
+# # =====================================================
+
+# # Display the plot
+# plt.show()
--- traoptlibrary/traopt_cost.py
+++ traoptlibrary/traopt_cost.py
@@ -3,6 +3,7 @@
 import abc
 import numpy as np
 from jax import jacfwd, hessian, jit
+from traopt_utilis import adjoint
 
 class BaseCost():
 
@@ -119,7 +120,7 @@ class AutoDiffCost(BaseCost):
                 This needs to be a function of x and u and must return a scalar.
                 Args:
                     x: Current state [state_size].
-                    u: Current control [action_size]. None if terminal.
+                    u: Current control [action_size].
                     i: Current time step.
             l_terminal: Function for terminal cost.
                 This needs to be a function of x only and must retunr a scalar.
@@ -160,6 +161,252 @@ class AutoDiffCost(BaseCost):
     def action_size(self):
         """Action size."""
         return self._action_size
+    
+    def l(self, x, u, i, terminal=False):
+        """Instantaneous cost function.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            Instantaneous cost (scalar).
+        """
+        if terminal:
+            return self._l_terminal(x,i)
+
+        return self._l(x,u,i)
+
+    def l_x(self, x, u, i, terminal=False):
+        """Partial derivative of cost function with respect to x.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            dl/dx [state_size].
+        """
+        if terminal:
+            return self._l_x_terminal(x,i)
+
+        return self._l_x(x,u,i)
+
+    def l_u(self, x, u, i, terminal=False):
+        """Partial derivative of cost function with respect to u.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            dl/du [action_size].
+        """
+        if terminal:
+            # Not a function of u, so the derivative is zero.
+            return np.zeros(self._action_size)
+
+        return self._l_u(x,u,i)
+
+    def l_xx(self, x, u, i, terminal=False):
+        """Second partial derivative of cost function with respect to x.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            d^2l/dx^2 [state_size, state_size].
+        """
+        if terminal:
+            return self._l_xx_terminal(x,i)
+
+        return self._l_xx(x,u,i)
+
+    def l_ux(self, x, u, i, terminal=False):
+        """Second partial derivative of cost function with respect to u and x.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            d^2l/dudx [action_size, state_size].
+        """
+        if terminal:
+            # Not a function of u, so the derivative is zero.
+            return np.zeros((self._action_size, self._state_size))
+
+        return self._l_ux(x,u,i)
+
+    def l_uu(self, x, u, i, terminal=False):
+        """Second partial derivative of cost function with respect to u.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            d^2l/du^2 [action_size, action_size].
+        """
+        if terminal:
+            # Not a function of u, so the derivative is zero.
+            return np.zeros((self._action_size, self._action_size))
+
+        return self._l_uu(x,u,i)
+    
+
+class ErrorStateSE3LieAlgebraAutoDiffQuadraticCost(BaseCost):
+
+    """Instantaneous Stage Cost defined on Lie Algebra for SE(3) Error-state Dynamics.
+        - Implemented with Jax for Autodiff.
+        - Quadratic form
+
+    """
+
+    def __init__(self, Q, R, P, xi_ref,
+                 state_size=(6,6), action_size=6, **kwargs):
+        """Constructs an AutoDiffCost.
+
+        Args:
+            Q: State weighting matrix for the stage cost. Shape: [state_size, state_size].
+                This matrix penalizes the state deviation from the reference at each time step.
+            R: Control weighting matrix for the stage cost. Shape: [action_size, action_size].
+                This matrix penalizes the magnitude of control inputs at each time step.
+            P: State weighting matrix for the terminal cost. Shape: [state_size, state_size].
+                This matrix penalizes the state deviation from the reference at the final time step.
+            xi_ref: List of velocity reference, described in Lie Algebra,
+                 (N, velocity_size, 1)
+            state_size: Tuple of State variable dimension, 
+                ( error state size, velocity state size ).
+            action_size: Input variable dimension.
+            **kwargs: Additional keyword-arguments to pass to
+                `theano.function()`.
+        """
+        self._state_size = state_size[0] + state_size[1]
+        self._error_state_size = state_size[0] 
+        self._vel_state_size = state_size[1] 
+        self._action_size = action_size
+
+        self._xi_ref = xi_ref
+        self._Q = Q
+        self._R = R
+        self._P = P
+
+        self._l = jit(self._l)
+
+        self._l_x = jit(jacfwd(self._l))
+        self._l_u = jit(jacfwd(self._l, argnums=1))
+
+        self._l_xx = jit(hessian(self._l, argnums=0))
+        self._l_ux = jit(jacfwd( jacfwd(self._l, argnums=1) ))
+        self._l_uu = jit(hessian(self._l, argnums=1))
+
+        # Terminal cost only depends on x, so we only need to evaluate the x
+        # partial derivatives.
+
+        self._l_terminal = jit(self._l_terminal)
+        self._l_x_terminal = jit(jacfwd(self._l_terminal))
+        self._l_xx_terminal = jit(hessian(self._l_terminal))
+
+        super(ErrorStateSE3LieAlgebraAutoDiffQuadraticCost, self).__init__()
+
+    @property
+    def state_size(self):
+        """State size."""
+        return self._state_size
+    
+    @property
+    def error_state_size(self):
+        """Error-state size."""
+        return self._error_state_size
+
+    @property
+    def vel_state_size(self):
+        """Velocity state size."""
+        return self._vel_state_size
+
+    @property
+    def action_size(self):
+        """Action size."""
+        return self._action_size
+    
+    @property
+    def Q(self):
+        """State cost coefficient matrix in the quadratic stage cost."""
+        return self._Q
+    
+    @property
+    def R(self):
+        """Input cost coefficient matrix in the quadratic stage cost."""
+        return self._R
+    
+    @property
+    def P(self):
+        """State cost coefficient matrix in the quadratic terminal cost."""
+        return self._P
+    
+    def xi_ref(self, i) :
+        """Return the Lie Algebra velocity xi reference xi_ref at time index i."""
+        return self._xi_ref[i]
+    
+    def _l(self, x, u, i ):
+        """Instantaneous cost function.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+
+        Returns:
+            Instantaneous cost (scalar).
+        """
+        
+        Ct = np.block([
+            [ np.identity( self.error_state_size ), np.zeros((self.vel_state_size,self.vel_state_size)) ],
+            [ -adjoint( self.xi_ref(i) ), np.identity( self.vel_state_size ) ],
+        ])
+        dt = np.vstack(
+            ( np.zeros((self.error_state_size,1)), self.xi_ref(i) )
+        )
+        yt = Ct @ x - dt
+        
+        return yt.T @ self.Q @ yt + u.T @ self.R @ u
+    
+    def _l_terminal(self, x, i):
+        """Terminal cost function.
+
+        Args:
+            x: Current state [state_size].
+            i: Current time step.
+
+        Returns:
+            Terminal cost (scalar).
+        """
+        
+        Ct = np.block([
+            [ np.identity( self.error_state_size ), np.zeros((self.vel_state_size,self.vel_state_size)) ],
+            [ -adjoint( self.xi_ref(i) ), np.identity( self.vel_state_size ) ],
+        ])
+        dt = np.vstack(
+            ( np.zeros((self.error_state_size,1)), self.xi_ref(i) )
+        )
+        yt = Ct @ x - dt
+        
+        return yt.T @ self.P @ yt
+
     
     def l(self, x, u, i, terminal=False):
         """Instantaneous cost function.
